d26879bb8de414fd2561d463324d403d8a05c5d7
Haizard <haithammisape@gmail.com>
2026-03-01 03:41:23 -0800

    Login Page & Footer Fixed!

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -39,8 +39,8 @@ const Footer = () => {
           muted
           className="absolute right-0 top-0 h-full overflow-hidden w-full object-cover z-[-1]"
         >
-            <source src={NatureVid} type="video/mp4" />
-            </video>
+          <source src={NatureVid} type="video/mp4" />
+        </video>
         <div className="container">
           <div className="grid md:grid-cols-3 py-5 bg-white/80 backdrop-blur-sm rounded-t-xl">
             <div className="py-8 px-4">
@@ -49,59 +49,59 @@ const Footer = () => {
                 {/* TravelloGo */}
               </h1>
               <p className="text-sm">
-              At  Makolo Adventure Tours, we craft unforgettable travel experiences with a focus on stunning destinations and exceptional service. Join us to explore the world's most breathtaking landscapes and make lasting memories.
+                At  Makolo Adventure Tours, we craft unforgettable travel experiences with a focus on stunning destinations and exceptional service. Join us to explore the world's most breathtaking landscapes and make lasting memories.
               </p>
               <br />
               <div className="flex items-center gap-3">
-  <FaEnvelope className="text-2xl" />
-  <a 
-    href="mailto:makoloadventuresafaris@gmail.com" 
-    className="text-blue-600 hover:text-blue-800 transition-colors duration-300 hover:text-red-700 transition-colors duration-500"
-  >
-    makoloadventuresafaris@gmail.com
-  </a>
-</div>
+                <FaEnvelope className="text-2xl" />
+                <a
+                  href="mailto:makoloadventuresafaris@gmail.com"
+                  className="text-blue-600 hover:text-blue-800 transition-colors duration-300 hover:text-red-700 transition-colors duration-500"
+                >
+                  makoloadventuresafaris@gmail.com
+                </a>
+              </div>
 
               <div className="flex items-center gap-3 mt-3">
-  <FaMobileAlt className="text-2xl" />
-  <a 
-    href="tel:+255710887798" 
-    className="text-blue-600 hover:text-blue-800 transition-colors duration-300 hover:text-red-700 transition-colors duration-500"
-  >
-    +255 710887798
-  </a>
-</div>
+                <FaMobileAlt className="text-2xl" />
+                <a
+                  href="tel:+255710887798"
+                  className="text-blue-600 hover:text-blue-800 transition-colors duration-300 hover:text-red-700 transition-colors duration-500"
+                >
+                  +255 710887798
+                </a>
+              </div>
 
               {/* social handles */}
               <div>
-              <div className="flex items-center gap-3 mt-6">
-  <a 
-    href="https://www.instagram.com/makoloafrika?igsh=aXV3NXF5OTN1Z3oz//https://www.tripadvisor.co.uk/Profile/makolosafari" 
-    target="_blank" 
-    rel="noopener noreferrer"
-    className="text-red-600 hover:text-gray-800 transition-colors duration-300"
-  >
-    <FaInstagram className="text-3xl" />
-  </a>
+                <div className="flex items-center gap-3 mt-6">
+                  <a
+                    href="https://www.instagram.com/makoloafrika?igsh=aXV3NXF5OTN1Z3oz//https://www.tripadvisor.co.uk/Profile/makolosafari"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-red-600 hover:text-gray-800 transition-colors duration-300"
+                  >
+                    <FaInstagram className="text-3xl" />
+                  </a>
 
-  <a 
-    href="https://www.linkedin.com/in/makoloadventure-safaris-666a13322?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" 
-    target="_blank" 
-    rel="noopener noreferrer"
-    className="text-red-600 hover:text-gray-800 transition-colors duration-300"
-  >
-    <FaLinkedin className="text-3xl" />
-  </a>
-  
-  <a 
-  href="https://www.tripadvisor.co.uk/Profile/makolosafari" 
-  target="_blank" 
-  rel="noopener noreferrer"
-  className="text-red-600 hover:text-gray-800 transition-colors duration-300"
->
-  <FaTripadvisor className="text-3xl" />
-</a>
-</div>
+                  <a
+                    href="https://www.linkedin.com/in/makoloadventure-safaris-666a13322?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-red-600 hover:text-gray-800 transition-colors duration-300"
+                  >
+                    <FaLinkedin className="text-3xl" />
+                  </a>
+
+                  <a
+                    href="https://www.tripadvisor.co.uk/Profile/makolosafari"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-red-600 hover:text-gray-800 transition-colors duration-300"
+                  >
+                    <FaTripadvisor className="text-3xl" />
+                  </a>
+                </div>
 
               </div>
             </div>
@@ -113,7 +113,7 @@ const Footer = () => {
                   </h1>
                   <ul className="flex flex-col gap-3">
                     {FooterLinks.map((link) => (
-                      <li className="cursor-pointer hover:translate-x-1 duration-300 hover:!text-primary space-x-1 text-gray-700 dark:text-gray-200">
+                      <li key={link.title} className="cursor-pointer hover:translate-x-1 duration-300 hover:!text-primary space-x-1 text-gray-700 dark:text-gray-200">
                         <Link
                           to={link.link}
                           onClick={() => window.scrollTo(0, 0)}
@@ -129,40 +129,39 @@ const Footer = () => {
               <div>
                 <div className="py-8 px-4">
                   <h1 className="text-xl font-bold text-justify sm:text-left mb-3">
-                    Important Links
+                    Company
                   </h1>
                   <ul className="flex flex-col gap-3">
-                    {FooterLinks.map((link) => (
-                      <li className="cursor-pointer hover:translate-x-1 duration-300 hover:!text-primary space-x-1 text-gray-700 dark:text-gray-200">
-                        <Link
-                          to={link.link}
-                          onClick={() => window.scrollTo(0, 0)}
-                        >
-                          <span>&#11162;</span>
-                          <span>{link.title}</span>
-                        </Link>
-                      </li>
-                    ))}
+                    <li className="cursor-pointer hover:translate-x-1 duration-300 hover:!text-primary space-x-1 text-gray-700 dark:text-gray-200">
+                      <Link to="/about" onClick={() => window.scrollTo(0, 0)}>
+                        <span>&#11162;</span>
+                        <span>About Us</span>
+                      </Link>
+                    </li>
+                    <li className="cursor-pointer hover:translate-x-1 duration-300 hover:!text-primary space-x-1 text-gray-700 dark:text-gray-200">
+                      <Link to="/best-places" onClick={() => window.scrollTo(0, 0)}>
+                        <span>&#11162;</span>
+                        <span>Best Places</span>
+                      </Link>
+                    </li>
                   </ul>
                 </div>
               </div>
               <div>
                 <div className="py-8 px-4">
                   <h1 className="text-xl font-bold text-justify sm:text-left mb-3">
-                    Important Links
+                    Admin
                   </h1>
                   <ul className="flex flex-col gap-3">
-                    {FooterLinks.map((link) => (
-                      <li className="cursor-pointer hover:translate-x-1 duration-300 hover:!text-primary space-x-1 text-gray-700 dark:text-gray-200">
-                        <Link
-                          to={link.link}
-                          onClick={() => window.scrollTo(0, 0)}
-                        >
-                          <span>&#11162;</span>
-                          <span>{link.title}</span>
-                        </Link>
-                      </li>
-                    ))}
+                    <li className="cursor-pointer hover:translate-x-1 duration-300 hover:!text-primary space-x-1 text-gray-700 dark:text-gray-200">
+                      <Link
+                        to="/login"
+                        onClick={() => window.scrollTo(0, 0)}
+                      >
+                        <span>&#11162;</span>
+                        <span>Login</span>
+                      </Link>
+                    </li>
                   </ul>
                 </div>
               </div>
