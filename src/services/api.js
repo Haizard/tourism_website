c9f1967bb0aa63a312101ae6c2e5be7c46a06cbc
Haizard <haithammisape@gmail.com>
2026-03-02 23:17:16 -0800

    Unified Vercel Deployment Setup

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_URL = window.location.hostname === "localhost"
   ? "http://localhost:5000/api"
-  : "https://tourism-website-inky.vercel.app/api";
+  : "/api";
 
 const API = axios.create({ baseURL: API_URL });
 
